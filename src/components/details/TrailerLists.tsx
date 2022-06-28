d563c8ac8703f1e40a310b1c7cd23755c54a2a42
Ashik-AD <looge321@gmail.com>
2022-06-28 17:57:53 +0545

    Removed slider

--- src/components/details/TrailerLists.tsx
+++ src/components/details/TrailerLists.tsx
@@ -1,91 +1,40 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-const SliderArrow = React.lazy(() => import("../slider/SliderArrow"));
-import Slider from "react-slick";
 import { RiPlayMiniFill } from "react-icons/ri";
-const settings = {
-  dots: false,
-  responsive: [
-    {
-      breakpoint: 1920,
-      settings: {
-        slidesToShow: 8,
-        slidesToScroll: 8,
-      },
-    },
-    {
-      breakpoint: 1620,
-      settings: {
-        slidesToShow: 6,
-        slidesToScroll: 6,
-      },
-    },
-    {
-      breakpoint: 1400,
-      settings: {
-        slidesToShow: 5,
-        slidesToScroll: 5,
-      },
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-      },
-    },
-    {
-      breakpoint: 670,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      },
-    },
-    {
-      breakpoint: 464,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-  ],
-};
+
 const TrailerLists: React.FC<{ url: string; color?: string }> = (props) => {
   const { data, loading, error } = useFetch(props.url);
   const navigation = useNavigate();
-  if (loading) return <h1>Loading...</h1>;
+  if (loading) return null;
   if (error) return <h1>Something went wrong</h1>;
   if (data === null) return <h1>No Videos</h1>;
   const handleNav = (id: string) => {
     navigation(`trailer/${id}`, { replace: true });
   };
   return (
-    <Slider
-      // shouldResetAutoplay={true}
-      {...settings}
-    >
+    <div className="grid col-2 sm:col-3 gap-10">
       {data.results.map((el: any) => (
         <div
           key={el.id}
-          className="flex content-center h-200 bg-cover cursor-pointer hover-scaleup transition-1 mx-6"
-          style={{
-            background: `url(https://i.ytimg.com/vi/${el.key}/hqdefault.jpg)`,
-            backgroundPosition: "center",
-          }}
+          className="relative flex h-150 content-center cursor-pointer mx-6 rounded-lg overflow-hidden"
           onClick={() => handleNav(el.key)}
         >
+          <div
+            style={{
+              background: `url(https://i.ytimg.com/vi/${el.key}/hqdefault.jpg)`,
+            }}
+            className="absolute h-full  w-full bg-center rounded-lg hover-scaleup z-0 transition-1"
+          />
           <span
-            className="rounded-full flex content-center text-heading overflow-hidden hover-fade-half"
+            className="rounded-full flex content-center text-heading overflow-hidden hover-fade-half z-1"
             style={{ background: props.color, height: 40, width: 40 }}
           >
             <RiPlayMiniFill />
           </span>
         </div>
       ))}
-    </Slider>
+    </div>
   );
 };
 
